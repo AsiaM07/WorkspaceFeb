d151fbaf4a6321581136a18d38ab1bbf4e822d7d
AsiaM07 <amcdo537@cougarmail.cccc.edu>
2025-02-04 08:26:32 -0500

    Add files via upload

---
+++ Workspace2025/Chap1act2/src/MovieQuote.java
@@ -0,0 +1,13 @@
+//Asia M 2/3 Chap1act2
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		System.out.println("The Menu");
+		System.out.println("I said, I don't like your food, and I would like to send it back.");
+		System.out.println("Margot");
+		System.out.println("Year 2022");
+
+	}
+
+}
---
+++ Workspace2025/SongLyricsFirst/src/Firstline.java
@@ -0,0 +1,12 @@
+//Asia M 2/1 Chap1Act1
+
+public class Firstline {
+
+	public static void main(String[] args) {
+		System.out.println("Make it to the high fashion");
+		System.out.println("Make it to the high fashion");
+		System.out.println("Make it to the high fashion");
+		
+	}
+
+}
